043b1214899a5432a25af5e81356cf940082ce59
felipemartinsbnf <felipemartinsbn@gmail.com>
2025-10-30 22:20:42 -0300

    feat: pesquisa de psicologos

--- paciente/views.py
+++ paciente/views.py
@@ -7,13 +7,20 @@ from psiacesso_main.models import Formacao
 def dashboard(request):
     #Logica para carregar 4 psicologos mais novos
     recentes = Psicologo.objects.order_by('-id')[:4]
+    
+    #Carregar as consultas do paciente logado (futuro)
     return render(request, 'dashboard.html', {'psicologos': recentes })
 
 #Carrega todos os psicologos disponiveis
 @login_required
 def view_all_psicologs(request):
-    psicologos = Psicologo.objects.all()
-    return render(request, 'psicologos_list.html', {'psicologos': psicologos })
+    query = request.GET.get('query', '')
+    
+    if query:
+        psicologos = Psicologo.objects.filter(usuario__first_name__icontains=query).distinct()
+    else:
+        psicologos = Psicologo.objects.filter()
+    return render(request, 'psicologos_list.html', {'psicologos': psicologos, 'query': query})
 
 #Carrega as informações de um psicologo para o paciente
 @login_required
