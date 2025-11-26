d870a7507a87d4bbd6c2d035a8dca1a72db556f0
Seu Nome Completo <seu-email-do-github@exemplo.com>
2025-11-25 22:26:50 -0300

    criação perfil paciente e psicologo

--- paciente/urls.py
+++ paciente/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard, view_all_psicologs, view_detail_psi, agendar, get_horarios_disponiveis, agenda_paciente_view, consulta_detalhe_paciente, cancelar_consulta_paciente
+from .views import dashboard, view_all_psicologs, view_detail_psi, agendar, get_horarios_disponiveis, agenda_paciente_view, consulta_detalhe_paciente, cancelar_consulta_paciente, perfil_paciente
 
 urlpatterns = [
     path('paciente/dashboard/', dashboard, name='user-dashboard'),
@@ -9,7 +9,10 @@ urlpatterns = [
     path('paciente/agenda', agenda_paciente_view, name='agenda_paciente'),
     path('paciente/cancelar/<int:consulta_id>', cancelar_consulta_paciente, name='cancelar_consulta'),
 
+    path('paciente/perfil/<int:pk>/', perfil_paciente, name='perfil-paciente'),
+
     #Modals
+
     path('paciente/consulta/detalhe/<int:consulta_id>', consulta_detalhe_paciente, name='consulta_detalhe_paciente'),
     
     path('api/horarios/<int:psicologo_id>/', get_horarios_disponiveis, name='get_horarios_disponiveis'),
